251e0cfcb761263615472d91d9006c11a0f5fe68
Priyanka Pandey <67233249+priyanka866@users.noreply.github.com>
2023-02-07 20:59:32 +0530

    Update long_running_python_kill.py

--- long_running_python_kill.py
+++ long_running_python_kill.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
-Created on Tue Feb  7 17:59:06 2023
+Created on Tue Feb  7 20:01:07 2023
 
 @author: priyankapandey
 """
+
 import os
-import sys
+import signal
 from datetime import timedelta, datetime
 import subprocess
 import re
@@ -57,11 +57,14 @@ with open(x, "r") as file:
                 tim_delta=datetime.strptime(current_time,"%H:%M:%S")-datetime.strptime(start_te,"%H:%M:%S")
                 error_type="Run Time : " + str(tim_delta)
                 if tim_delta > timedelta(seconds=10800):
+                    # terminating process
+                    os.kill(int(pid), signal.SIGKILL)
                     print(f"Time limit exceeded for {pid} for {cmd}")
                 else:
                     pass
             except ValueError:
-                pass                    
+                pass
+            
 
 os.remove(x)
 
